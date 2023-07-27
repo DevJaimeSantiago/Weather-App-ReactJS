e1021808b6a17ff0345ebcf4c2c91c963504e96f
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-07-27 12:02:54 -0500

    Estilos CSS en componente Forecast

--- src/components/forecast/Forecast.jsx
+++ src/components/forecast/Forecast.jsx
@@ -6,17 +6,20 @@ import {
 	AccordionItemPanel,
 } from "react-accessible-accordion";
 
+import "./forecast.css";
+
 const WEEK_DAYS = [
+	"Sunday",
 	"Monday",
 	"Tuesday",
 	"Wednesday",
 	"Thursday",
 	"Friday",
 	"Saturday",
-	"Sunday",
 ];
 const Forecast = ({ data }) => {
 	const dayInWeeek = new Date().getDay();
+	// console.log(dayInWeeek);
 	const forecastDays = WEEK_DAYS.slice(dayInWeeek, WEEK_DAYS.length).concat(
 		WEEK_DAYS.slice(0, dayInWeeek)
 	);
@@ -38,6 +41,12 @@ const Forecast = ({ data }) => {
 										className="icon-small"
 									/>
 									<label className="day">{forecastDays[index]}</label>
+									<label className="description">
+										{item.weather[0].description}
+									</label>
+									<label className="min-max">
+										{item.main.temp_min}°C / {item.main.temp_max}°C
+									</label>
 								</div>
 							</AccordionItemButton>
 						</AccordionItemHeading>
